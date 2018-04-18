9d516c66d2438ab1333cb392752712a33b0dcd40
Unknown <garifb@gmail.com>
2018-04-19 02:44:00 +0300

    Добавлена валидация хэштегов

--- js/handlers.js
+++ js/handlers.js
@@ -31,3 +31,40 @@ filtersFormOClose.addEventListener('click', function () {
   closeFiltersForm();
 });
 
+var hashTagsInput = filtersForm.querySelector('.text__hashtags');
+
+var checkForDuplicates = function (array) {
+  var isDuplicates = false;
+  var arrayObj = {};
+  for (var i = 0; i < array.length; i++) {
+    var str = array[i].toLowerCase();
+    if (!arrayObj[str]) {
+      arrayObj[str] = true;
+      isDuplicates = false;
+    } else {
+      isDuplicates = true;
+    }
+  }
+  console.log(isDuplicates);
+  return isDuplicates;
+};
+
+var onHashTagInput = function (evt) {
+  var inputTarget = evt.target;
+  var hashTags = inputTarget.value.split(' ');
+  var is = checkForDuplicates(hashTags);
+  if (hashTags.length > 5) {
+    inputTarget.setCustomValidity('Максимальное количество хэштегов - 5');
+  } if (is) {
+    inputTarget.setCustomValidity('Удалите повторяющиеся хэштеги ' + checkForDuplicates(hashTags));
+  }
+  hashTags.forEach(function (hashTag) {
+    if (hashTag.charAt(0) !== '#') {
+      inputTarget.setCustomValidity('Хештэг должен начинаться с символа #');
+    } else if (hashTag.length > 20) {
+      inputTarget.setCustomValidity('Максимальная длина одного хэштега - 20 символов');
+    }
+  });
+};
+
+hashTagsInput.addEventListener('change', onHashTagInput);
